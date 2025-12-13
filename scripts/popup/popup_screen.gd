5ec06ac83f0ed412deed5f6eb360e80e97732e36
Julien Pilleux <j.pilleux@gmail.com>
2025-12-13 16:58:33 +0100

    ADding screen shake

--- scripts/popup/popup_screen.gd
+++ scripts/popup/popup_screen.gd
@@ -1,20 +1,24 @@
 extends Node2D
 
-@onready var screen = $BrowserScreenTmp
+@onready var area = $AnimationPlayer/PopupArea
+@onready var anim = $"../CameraAnimation"
+
 var popup_1 = preload("res://scenes/popup/popup_1.tscn")
 var nb_popup = 0
+var nb_closed_popup = 0
 var elapsed_time = 0
-var zebi = 0
+
+var finished = false
 
 const FREQ_SEC: float = 1
-const MAX_POPUP: int = 100
-const CHAISPAS: int = 3
+var max_popup: int = 20
+const NB_POP_WHEN_ERROR: int = 3
 
-func _rand_pos(popup_size: Vector2) -> Vector2:
+func _rand_pos() -> Vector2:
 	randomize()
 	
-	var max_x = screen.position.x * 1.5
-	var max_y = screen.position.y * 1.5
+	var max_x = 850
+	var max_y = 550
 	var x = randf_range(100, max_x)
 	var y = randf_range(50, max_y)
 	
@@ -22,25 +26,37 @@ func _rand_pos(popup_size: Vector2) -> Vector2:
 
 
 func _wrong_choice() -> void:
-	for i in range(CHAISPAS):
+	anim.play("screen_shake")
+	for i in range(NB_POP_WHEN_ERROR):
 		_create_popup()
+	max_popup += NB_POP_WHEN_ERROR - 1
+
+
+func _close_popup() -> void:
+	nb_popup -= 1
+	nb_closed_popup += 1
 
 func _create_popup() -> void:
 	var popup = popup_1.instantiate()
 	var sprite = popup.get_child(0).get_child(0)
-	var width = sprite.texture.get_size().x
-	var height = sprite.texture.get_size().y
-	sprite.position = _rand_pos(Vector2(width, height))
-	sprite.z_index = zebi
-	add_child(popup)
-	popup.connect("popup_btn_close", func() : nb_popup -= 1)
-	popup.connect("popup_btn_accept", _wrong_choice)
+	var pos = _rand_pos()
+	sprite.position = pos
+	area.add_child(popup)
+	popup.popup_close.connect(_close_popup)
+	popup.popup_accept.connect(_wrong_choice)
 	
 
 func _process(delta: float) -> void:
 	elapsed_time += delta
 	
-	if nb_popup < MAX_POPUP and elapsed_time >= FREQ_SEC:
+	if finished:
+		print("GG mec")
+		queue_free()
+	
+	if nb_popup < max_popup and elapsed_time >= FREQ_SEC:
 		_create_popup()
 		elapsed_time = 0
 		nb_popup += 1
+		
+	if nb_closed_popup == max_popup:
+		finished = true
