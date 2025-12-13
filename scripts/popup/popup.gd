126344e07ccc832a4d5f7f55fbc40aeec6e438e8
Julien Pilleux <j.pilleux@gmail.com>
2025-12-13 16:58:33 +0100

    ADding screen shake

--- scripts/popup/popup.gd
+++ scripts/popup/popup.gd
@@ -1,15 +1,19 @@
 extends Node2D
 
-signal popup_btn_close
-signal popup_btn_accept
+signal popup_accept
+signal popup_close
 
-@onready var close_btn = $AnimationPlayer/PopupTemplate/CloseBtn
-@onready var accept_btn = $AnimationPlayer/PopupTemplate/AcceptBtn
+@onready var close_btn = $AnimationPlayer/Area2D/CloseBtn
+@onready var accept_btn = $AnimationPlayer/Area2D/AcceptBtn
 
 func _on_close() -> void:
-	emit_signal("popup_btn_close")
+	popup_close.emit()
+	queue_free()
+	
+func _on_accept() -> void:
+	popup_accept.emit()
 	queue_free()
 
 func _ready() -> void:
-	close_btn.connect("pressed", _on_close)
-	accept_btn.connect("pressed", func() : emit_signal("popup_btn_accept"))
+	close_btn.popup_btn_close.connect(_on_close)
+	accept_btn.popup_btn_accept.connect(_on_accept)
