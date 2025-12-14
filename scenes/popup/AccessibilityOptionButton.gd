6ae8e7d9e41ad3bc07334a48b1ea7d856798596c
VictorMusta <victorgrabowski33@gmail.com>
2025-12-14 12:46:40 +0100

    Accessibility added

---
+++ scenes/popup/AccessibilityOptionButton.gd
@@ -0,0 +1,9 @@
+extends Button
+
+@onready var canvasLayer = $"../.."
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func _pressed() -> void:
+	canvasLayer.visible = false
---
+++ scenes/popup/accessibility_button.gd
@@ -0,0 +1,7 @@
+extends Button
+
+@onready var accessibilityMenu = $"../../CanvasLayer"
+
+func _pressed() -> void:
+	self.accessibilityMenu.visible = true
+	
