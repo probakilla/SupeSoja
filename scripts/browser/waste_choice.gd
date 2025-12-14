307159663120539fe5744e3244900ae8d9812de6
Arthur Jarreau <arthur.jarreau@epitech.eu>
2025-12-14 10:04:11 +0100

    [ADD] browser game

---
+++ scripts/browser/waste_choice.gd
@@ -0,0 +1,40 @@
+extends Node
+
+@onready var buttons = $ChoiceButon1
+@onready var question = $Label
+@onready var button_one = $ChoiceButon1/Button
+@onready var button_two = $ChoiceButon1/ChoiceButon2/Button
+@onready var button_three = $ChoiceButon1/ChoiceButon2/ChoiceButon3/Button
+@onready var browser = $/root/BrowserS/Browser
+@onready var text_wrong = $/root/BrowserS/WrongChoice
+@onready var text_good = $/root/BrowserS/GoodChoice
+var action_choice = preload("res://scenes/browser/serveur_choice.tscn")
+
+func _button_pressed():
+	var scene = action_choice.instantiate()
+	scene.position = Vector2(-600, -150)
+	button_one.visible = false
+	question.visible = false
+	browser.add_child(scene)
+
+func _goodchoice_pressed() :
+	text_good.visible = true
+	buttons.visible = false
+	question.visible = false
+	await get_tree().create_timer(1.0).timeout
+	text_good.visible = false
+	_button_pressed()
+
+func _wrongchoice_pressed() :
+	text_wrong.visible = true
+	buttons.visible = false
+	question.visible = false
+	await get_tree().create_timer(1.0).timeout
+	text_wrong.visible = false
+	_button_pressed()
+
+func _ready() -> void:
+	button_one.connect("pressed", _wrongchoice_pressed)
+	button_two.connect("pressed", _wrongchoice_pressed)
+	button_three.connect("pressed", _goodchoice_pressed)
+	pass
